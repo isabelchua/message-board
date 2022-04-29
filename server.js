26e395b77e8a39c8f1e54c3300d16d1c4ed20a0c
isabelchua <isabelchua86@gmail.com>
2022-04-29 12:45:48 -0700

    auth setup

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.get("/", (req, res) => res.json({ msg: "Welcome to the Message Board!" }));
 
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/messages", require("./routes/messages"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
