8a885c443e41c3649900e57bb37b994e5309c9b4
isabelchua <isabelchua86@gmail.com>
2022-05-10 14:39:02 -0700

    pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">test</header>
-		</div>
+		<Router>
+			<>
+				<header className="App-header">test</header>
+				<Routes>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/about" component={About} />
+				</Routes>
+			</>
+		</Router>
 	);
 }
 
